2c3494eb21b65acc26bfd4fbaa0b82b7dfa8923d
Sofiya Yurova <sofiya.yurova@gmail.com>
2019-05-10 09:09:28 +0300

    Generate Jobs table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190510055538) do
+ActiveRecord::Schema.define(version: 20190510060632) do
+
+  create_table "jobs", force: :cascade do |t|
+    t.string "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
