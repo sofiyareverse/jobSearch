2137c264c8990799ccb7b76f30b01066ac066656
Sofiya Yurova <sofiya.yurova@gmail.com>
2019-05-10 12:37:23 +0300

    Add validations and limits for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,21 +4,45 @@ class User < ApplicationRecord
   has_many :notes
   has_and_belongs_to_many :jobs
 
+  validates :name, presence: true
+  validate :email_format
+  validates :phone,
+            numericality: true,
+            length: { minimum: 0, maximum: 50 },
+            allow_blank: true
+
   def self.import(file)
     CSV.foreach(file.path, headers: true) do |row|
       new_rows_hash = modefy_file_headers(row)
       
-      User.create!(
-        name: new_rows_hash[:name],
-        email: new_rows_hash[:email],
-        phone: new_rows_hash[:phone]
-      )
+      if User.where(email: new_rows_hash[:email]).count > 0
+        true
+      else
+        User.create!(
+          name: new_rows_hash[:name],
+          email: new_rows_hash[:email],
+          phone: self.try(:phone) || new_rows_hash[:phone]
+        )
+      end
     end
   end
 
-  def modefy_file_headers(row)
+  private
+
+  def self.modefy_file_headers(row)
     row.inject({}) do |h, (k, v)|
       h[k.downcase.tr(' ', '_').delete('-').to_sym] = v; h
+    end
+  end
+
+  def email_format
+    unless email.match?(URI::MailTo::EMAIL_REGEXP)
+      if email.match?('^[+]*[(]{0,1}[0-9]{1,4}[)]{0,1}[-\s\./0-9]*$')
+        self.phone = email
+        self.email = nil
+      else
+        errors.add(:email, 'Wrong format')
+      end
     end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190510062612) do
+ActiveRecord::Schema.define(version: 20190510083313) do
 
   create_table "jobs", force: :cascade do |t|
     t.string "title"
@@ -35,7 +35,7 @@ ActiveRecord::Schema.define(version: 20190510062612) do
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
-    t.integer "phone"
+    t.integer "phone", limit: 8
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
