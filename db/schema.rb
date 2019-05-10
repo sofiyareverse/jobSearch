a8d95a01e307c1bca70b6e771d1a6525e7c63363
Sofiya Yurova <sofiya.yurova@gmail.com>
2019-05-10 09:19:25 +0300

    Add reference notes belong to user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190510061000) do
+ActiveRecord::Schema.define(version: 20190510061506) do
 
   create_table "jobs", force: :cascade do |t|
     t.string "title"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 20190510061000) do
     t.string "message"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
