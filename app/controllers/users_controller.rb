618dcdccb0e8247a4be5c5d83a6b16dfd021434e
Sofiya Yurova <sofiya.yurova@gmail.com>
2019-05-10 10:50:33 +0300

    Add import for User

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,11 @@
+class UsersController < ApplicationController
+  def index
+    @users = User.all 
+  end
+
+  def import
+    User.import(params[:file])
+
+    redirect_to root_url
+  end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,24 @@
 class User < ApplicationRecord
+  require 'csv'
+
   has_many :notes
   has_and_belongs_to_many :jobs
+
+  def self.import(file)
+    CSV.foreach(file.path, headers: true) do |row|
+      new_rows_hash = modefy_file_headers(row)
+      
+      User.create!(
+        name: new_rows_hash[:name],
+        email: new_rows_hash[:email],
+        phone: new_rows_hash[:phone]
+      )
+    end
+  end
+
+  def modefy_file_headers(row)
+    row.inject({}) do |h, (k, v)|
+      h[k.downcase.tr(' ', '_').delete('-').to_sym] = v; h
+    end
+  end
 end
